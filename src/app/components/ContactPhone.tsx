8ea214001f63c57a8886c3c926fea0dcc721b3f8
Aaqil Ramzeen <aaqil.ramzeen@gmail.com>
2023-12-03 20:25:08 +0530

    update .env variables

--- src/app/components/ContactPhone.tsx
+++ src/app/components/ContactPhone.tsx
@@ -8,7 +8,7 @@ const ContactPhone = () => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="w-4 h-4 mr-1"
         >
@@ -27,7 +27,7 @@ const ContactPhone = () => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="w-4 h-4 mr-1"
         >
